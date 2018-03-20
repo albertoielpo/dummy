c3159702d63adab9c26d735f55f2caeaec15703b
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2018-03-20 12:57:32 +0100

    random utils +++ network utils

--- src/dummy/RandomUtils.java
+++ src/dummy/RandomUtils.java
@@ -1,5 +1,6 @@
 package dummy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -9,10 +10,12 @@ import java.util.UUID;
 public class RandomUtils {
 
 	/* Object */
+	/* String */
 	public static String randomString() {
 		return UUID.randomUUID().toString();
 	}
 		
+	/* Integer */
 	public static Integer randomInteger() {
 		Random randomGenerator = new Random();
 		return new Integer(randomGenerator.nextInt());
@@ -31,6 +34,41 @@ public class RandomUtils {
 		return random;
 	}
 	
+	/**
+	 * Generate a random Integer lower than maxValue param 
+	 * @param maxValue
+	 * @return
+	 */
+	public static Integer randomIntegerLowerThan(Integer maxValue) {
+		Random randomGenerator = new Random();
+		return randomGenerator.nextInt(maxValue);
+	}
+	
+	/**
+	 * Generate a list of random Integer lower than maxValue param. 
+	 * The numbers of list element's are definited with qty param
+	 * @param qty
+	 * @param maxValue
+	 * @return
+	 */
+	public static List<Integer> randomListIntegerLowerThan(Integer qty, Integer maxValue){
+		List<Integer> res = new ArrayList<Integer>();
+		for(int i=0; i<qty; i++)
+			res.add(randomIntegerLowerThan(maxValue));
+		return res;
+	}
+	
+	/**
+	 * Generate a list of random Integer
+	 * The numbers of list element's are definited with qty param
+	 * @param qty
+	 * @return
+	 */
+	public static List<Integer> randomListInteger(Integer qty){
+		return randomListIntegerLowerThan(qty, Integer.MAX_VALUE);
+	}
+	
+	/* Long */
 	public static Long randomLong() {
 		Random randomGenerator = new Random();
 		return new Long(randomGenerator.nextLong());
@@ -49,6 +87,7 @@ public class RandomUtils {
 		return Math.abs(randomLong());
 	}
 	
+	/* Float */
 	public static Float randomFloat() {
 		Random randomGenerator = new Random();
 		return new Float(randomGenerator.nextFloat());
@@ -67,6 +106,7 @@ public class RandomUtils {
 		return Math.abs(randomFloat());
 	}
 	
+	/* Double */
 	public static Double randomDouble() {
 		Random randomGenerator = new Random();
 		return new Double(randomGenerator.nextDouble());
@@ -85,15 +125,18 @@ public class RandomUtils {
 		return Math.abs(randomDouble());
 	}
 	
+	/* Date */
 	public static Date randomDate() {
 		return new Date();
 	}
 	
+	/* Boolean */
 	public static Boolean randomBoolean() {
 		Random randomGenerator = new Random();
 		return new Boolean(randomGenerator.nextBoolean());
 	}
 	
+	/* Short */
 	public static Short randomShort() {
 		Random randomGenerator = new Random();		
 		return new Short((short) randomGenerator.nextInt(Short.MAX_VALUE + 1));
@@ -104,6 +147,7 @@ public class RandomUtils {
 		return new Short((short) ii.intValue());
 	}
 	
+	/* ***************************************************************************** */
 	/* primitive */
 	public static int randomint() {
 		Random randomGenerator = new Random();
@@ -171,6 +215,5 @@ public class RandomUtils {
 		Random randomGenerator = new Random();
 		return (short) randomGenerator.nextInt(Short.MAX_VALUE + 1);
 	}
-	
 	
 }
--- src/dummy/Utils.java
+++ src/dummy/Utils.java
@@ -1,12 +1,108 @@
 package dummy;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class Utils {
     
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	/**
+	 * 
+	 * @param collection
+	 * @return
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static <T> Collection<T> emptyIfNull(final Collection<T> collection) {        
 		return collection == null ? new ArrayList() : collection;
     }
+	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 * @throws Exception
+	 */
+	public static String serializeArrayAsList(Object[] input) throws Exception {
+		if(input != null && input.length > 0) {
+			List<Object> list = new ArrayList<Object>(Arrays.asList(input));
+			ObjectMapper mapper = new ObjectMapper();		
+			return mapper.writeValueAsString(list);
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 * @throws Exception
+	 */
+	public static String serializeObject(Object input) throws Exception {
+		if(input != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			return mapper.writeValueAsString(input);	
+		}
+			       
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param input
+	 * @param clazz
+	 * @return
+	 * @throws Exception
+	 */
+	public static Object deserialize(String input, Class<?> clazz) throws Exception {
+		if(input != null && clazz != null) {
+			ObjectMapper mapper = new ObjectMapper();
+			return mapper.readValue(input, clazz);
+		}
+
+		return null;
+	}
+	
+	public static String generateUUID3(String str) {
+		if(str != null)
+			return UUID.nameUUIDFromBytes(str.getBytes()).toString();
+		return null;
+	}
+	
+	public static InetAddress getInetAddress() throws Exception {
+		return InetAddress.getLocalHost();
+	}
+	
+	public static String getIp() throws Exception {
+		return getInetAddress().getHostAddress();
+	}
+	
+	public static String getHostname() throws Exception {
+		return getInetAddress().getHostName();
+	}
+	
+	public static NetworkInterface getNetworkInterface() throws Exception {
+		return NetworkInterface.getByInetAddress(getInetAddress());
+	}
+	
+	public static byte[] getHardwareAddress() throws Exception {
+		return getNetworkInterface().getHardwareAddress();
+	}
+	
+	public static String getMacAddress() {
+		try {
+			return new String(getHardwareAddress(), StandardCharsets.UTF_8);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	
 }
