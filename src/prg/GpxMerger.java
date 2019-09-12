de3a4c48deeb453eb38cb44f0344125968abde4c
Alberto Ielpo <alberto.ielpo@ennova-research.com>
2019-09-12 15:18:25 +0200

    fix

--- src/prg/GpxMerger.java
+++ src/prg/GpxMerger.java
@@ -18,8 +18,10 @@ import utils.Utils;
 public class GpxMerger {
 
 	private static String GPX_FILES_PATH = ".";
+	private static String OUTPUT_FILE_NAME = "merge.gpx";
 	
 	/**
+	 * Get all the gpx files in one directory
 	 * @param path
 	 * @return
 	 */
@@ -29,7 +31,7 @@ public class GpxMerger {
 		if(f.isDirectory()) {
 			File[] files = f.listFiles();
 			for(File gpx: files) {
-				if(gpx.isFile() && gpx.getAbsolutePath().endsWith(".gpx")) {
+				if(gpx.isFile() && gpx.getAbsolutePath().toLowerCase().endsWith(".gpx")) {
 					allGpx.add(gpx);
 				}
 			}
@@ -38,7 +40,7 @@ public class GpxMerger {
 	}
 	
 	/**
-	 * 
+	 * Order a list of file using <time> tag. If not found into the gpx use current unix timestamp
 	 * @param files
 	 * @return
 	 * @throws IOException
@@ -47,9 +49,13 @@ public class GpxMerger {
 		List<File> res = new ArrayList<File>();
 		Map<Long, File> mapFiles = new TreeMap<Long, File>();
 		for(File f: files) {
-			String str = FileUtils.fileToString(f.getAbsolutePath());
-			String d = str.substring(str.indexOf("<time>") + "<time>".length(), str.indexOf("</time>"));
-			Long unixDate = Utils.mapAsUnixDate(d.replaceAll("T", " ").replaceAll("Z",""), "yyyy-MM-dd hh:mm:ss", "GMT");
+			String strLower = FileUtils.fileToString(f.getAbsolutePath()).toLowerCase();
+			long unixDate = new Date().getTime();
+			if(strLower.indexOf("<time>") > -1) {
+				String d = strLower.substring(strLower.indexOf("<time>") + "<time>".length(), strLower.indexOf("</time>"));
+				unixDate = Utils.mapAsUnixDate(d.replaceAll("t", " ").replaceAll("z",""), "yyyy-MM-dd hh:mm:ss", "GMT");
+			}
+			
 			mapFiles.put(unixDate, f);
 		}
 		
@@ -60,7 +66,7 @@ public class GpxMerger {
 	}
 	
 	/**
-	 * 
+	 * Get the head (including trkseg track) and the foot of the gpx
 	 * @param f
 	 * @return
 	 * @throws IOException
@@ -68,21 +74,22 @@ public class GpxMerger {
 	private static Map<String, String> getHeadFoot(File f) throws IOException {
 		Map<String, String> res = new HashMap<String, String>();
 		String str = FileUtils.fileToString(f.getAbsolutePath());
-		res.put("head", str.substring(0, str.indexOf("</trkseg>")));
-		res.put("foot", str.substring(str.indexOf("</trkseg>"), str.length()));
+		String strLower = str.toLowerCase();
+		res.put("head", str.substring(0, strLower.indexOf("</trkseg>")));
+		res.put("foot", str.substring(strLower.indexOf("</trkseg>"), str.length()));
 		return res;
 	}
 	
 	/**
-	 * 
+	 * Get the content between trkseg tag
 	 * @param f
 	 * @return
 	 * @throws IOException
 	 */
 	private static String getTrkseg(File f) throws IOException {
 		String str = FileUtils.fileToString(f.getAbsolutePath());
-		str.substring(str.indexOf("<trkseg>") + "<trkseg>".length(), str.indexOf("</trkseg>"));
-		return str;
+		String strLower = str.toLowerCase();
+		return str.substring(strLower.indexOf("<trkseg>") + "<trkseg>".length(), strLower.indexOf("</trkseg>"));
 	}
 	
 	/**
@@ -99,24 +106,24 @@ public class GpxMerger {
 				gpxFilesPath = args[0];
 			
 			/* get all files */
-			List<File> allGpxFiles = getAllFiles(gpxFilesPath);
+			List<File> allGpxFiles = GpxMerger.getAllFiles(gpxFilesPath);
 			if(allGpxFiles.size() > 1) {
 				
 				/* order files by date - using <time>2019-09-12T06:38:31Z</time> */
-				List<File> orderedGpxFiles = orderFiles(allGpxFiles);
+				List<File> orderedGpxFiles = GpxMerger.orderFiles(allGpxFiles);
 					
 				/* get head of first file and create the merged file called content */
-				Map<String,String> headFoot = getHeadFoot(orderedGpxFiles.get(0));
+				Map<String,String> headFoot = GpxMerger.getHeadFoot(orderedGpxFiles.get(0));
 				content.append(headFoot.get("head"));
 				
 				/* put inside the content what is inside the trkseg segment of all the files */
 				for(int ii=1; ii<orderedGpxFiles.size(); ii++)
-					content.append(getTrkseg(orderedGpxFiles.get(ii)));
+					content.append(GpxMerger.getTrkseg(orderedGpxFiles.get(ii)));
 				
 				/* append the foot */
 				content.append(headFoot.get("foot"));
 				
-				FileUtils.writeFile(gpxFilesPath + "\\res.gpx", content.toString());
+				FileUtils.writeFile(gpxFilesPath + "\\" + OUTPUT_FILE_NAME, content.toString());
 			} else {
 				System.out.println("Found " + allGpxFiles.size() + " gpx files. Nothing to do");
 			}
