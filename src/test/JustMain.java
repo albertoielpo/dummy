ce2963b58acb2961972b18209e915600ab0a3da6
Alberto Ielpo <alberto@ielpo.net>
2020-06-11 09:11:52 +0200

    --

--- src/test/JustMain.java
+++ src/test/JustMain.java
@@ -1,25 +1,9 @@
 package test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class JustMain {
 
-	public void pippo(long a, long b) {
-		
-	}	
-	
-	
 	public static void main(String... args) throws Exception {
-		
-		Map<String, B> q = new HashMap<String, B>();
-		Map<String, A> w  =new HashMap<String, A>();
-		
-		A a = new A();
-		B b = new B();
-		
-		q.put("b", b);
-		
 		
 	}
 
